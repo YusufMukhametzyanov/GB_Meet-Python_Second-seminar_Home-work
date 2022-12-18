a7d5fe4daae322ec279389c47ba91f624b0787b3
Юрий Мухаметзянов <YusufuMukhametzyanov@yandex.ru>
2022-12-18 21:00:15 +0500

    Redactor

--- First task.py
+++ First task.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Пример:
+# 6782 -> 23
+# 0,56 -> 11
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+number = input('Enter number: ')
+sum = 0
+for a in number:
+    if a.isdigit():
+        sum += int(a)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(f"{number} -> ", sum)
